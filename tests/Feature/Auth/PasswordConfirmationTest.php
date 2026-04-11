fa80cee980d0a5885736832404ced01691e56add
amhrn <mhrn77@gmail.com>
2026-04-11 17:19:06 +0500

    chore: clean up test files

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -1,20 +1,5 @@
 <?php
 
-use App\Models\User;
-use Inertia\Testing\AssertableInertia as Assert;
-
-test('confirm password screen can be rendered', function () {
-    $user = User::factory()->create();
-
-    $response = $this->actingAs($user)->get(route('password.confirm'));
-
-    $response->assertOk();
-
-    $response->assertInertia(fn (Assert $page) => $page
-        ->component('auth/confirm-password'),
-    );
-});
-
 test('password confirmation requires authentication', function () {
     $response = $this->get(route('password.confirm'));
 
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -11,15 +11,3 @@ test('registration screen can be rendered', function () {
 
     $response->assertOk();
 });
-
-test('new users can register', function () {
-    $response = $this->post(route('register.store'), [
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
-    ]);
-
-    $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
-});
