0795ddbbeb76a299ea776838920697a5ed913cd5
amhrn <mhrn77@gmail.com>
2026-04-11 12:59:59 +0500

    feat: seed required users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = ['User A', 'User B', 'Super Admin'];
+        foreach ($users as $user) {
+            User::factory()->create([
+                'name' => $user,
+                'email' => strtolower(str_replace(' ', '.', $user)).'@example.com',
+            ]);
+        }
     }
 }
