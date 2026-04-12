1e20cb17d27370885c0ef07b8d816436ff6e701e
amhrn <mhrn77@gmail.com>
2026-04-12 17:33:34 +0500

    feat: add documents route and update sidebar navigation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocumentController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,8 @@ Route::inertia('/', 'welcome', [
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+    Route::resource('documents', DocumentController::class);
 });
 
 require __DIR__.'/settings.php';
