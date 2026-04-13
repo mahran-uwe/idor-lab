a122ac9b11a271b03a5dc7ef8b1c983c39932fd5
amhrn <mhrn77@gmail.com>
2026-04-13 14:33:06 +0500

    feat(DatabaseSeeder): create invoices for users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            ProductSeeder::class,
+        ]);
+        
         $users = ['User A', 'User B', 'Super Admin'];
         foreach ($users as $user) {
             $user = User::factory()->create([
@@ -30,6 +34,42 @@ class DatabaseSeeder extends Seeder
                     'path' => storage_path('app/demo/documents/Sample Document.pdf'),
                 ],
             ]);
+
+            $invoices = $user->invoices()->createMany([
+                [
+                    'invoice_number' => 'INV-'.str_pad($user->id * 2 - 1, 4, '0', STR_PAD_LEFT),
+                    'due_date' => now()->addDays(30),
+                    'status' => 0,
+                    'subtotal' => 100,
+                    'gst' => 10,
+                    'total' => 110,
+                ],
+                [
+                    'invoice_number' => 'INV-'.str_pad($user->id * 2, 4, '0', STR_PAD_LEFT),
+                    'due_date' => now()->addDays(30),
+                    'status' => 0,
+                    'subtotal' => 200,
+                    'gst' => 20,
+                    'total' => 220,
+                ],
+            ]);
+
+            foreach ($invoices as $invoice) {
+                $invoice->items()->createMany([
+                    [
+                        'product_id' => 1,
+                        'quantity' => 2,
+                        'unit_price' => 50,
+                        'line_total' => 100,
+                    ],
+                    [
+                        'product_id' => 2,
+                        'quantity' => 1,
+                        'unit_price' => 100,
+                        'line_total' => 100,
+                    ],
+                ]);
+            }
         }
     }
 }
